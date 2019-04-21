bae9212f88a1259429beabdd1b6301e4d4bae8fe
johnniereg <johnniereg@gmail.com>
2019-04-21 16:30:59 -0700

    Add test Netlify form

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,5 +4,25 @@ export default () => (
   <div>
     <h1>Kelowna Auto Loans</h1>
     <p>We know bad things happen to good people.</p>
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Your Role: <select name="role[]" multiple>
+          <option value="leader">Leader</option>
+          <option value="follower">Follower</option>
+        </select></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
   </div>
 )
