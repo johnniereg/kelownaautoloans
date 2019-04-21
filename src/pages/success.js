704ef550342c6c9f313b99e972e870aff024421f
johnniereg <johnniereg@gmail.com>
2019-04-21 16:41:44 -0700

    Basic success page

---
+++ src/pages/success.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Success = props => (
+
+    <div id="main" className="alt">
+      <section id="one">
+        <div className="inner">
+          <header className="major">
+            <h1>Success/Thank You Page</h1>
+          </header>
+          <span className="image main">
+          </span>
+          <p>Thank you for contacting us!</p>
+        </div>
+      </section>
+    </div>
+)
+
+export default Success
