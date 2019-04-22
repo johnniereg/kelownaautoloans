65e013706f6f37c93e376c7bc03b9042889458ce
johnniereg <johnniereg@gmail.com>
2019-04-21 18:10:53 -0700

    Add rough styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 export default () => (
-  <div>
+  <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
     <h1>Kelowna Auto Loans</h1>
     <p>We know bad things happen to good people.</p>
     <form
@@ -13,68 +13,68 @@ export default () => (
     >
       <input type="hidden" name="bot-field" />
 
-      <div>
+      <div className="input-wrapper">
         <label htmlFor="firstName">First Name</label>
         <input type="text" name="firstName" id="firstName" required />
       </div>
 
-      <div>
+      <div className="input-wrapper">
         <label htmlFor="lastName">Last Name</label>
         <input type="text" name="lastName" id="lastName" required />
       </div>
 
-      <div>
+      <div className="input-wrapper">
         <label htmlFor="phone">Phone</label>
         <input type="tel" name="phone" id="phone" required />
       </div>
 
-      <div>
+      <div className="input-wrapper">
         <label htmlFor="email">Email</label>
         <input type="email" name="email" id="email" />
       </div>
 
-      <div>
+      <div className="input-wrapper">
         <label htmlFor="birthdate">Date of birth</label>
         <input type="date" name="birthdate" id="birthdate" />
       </div>
 
-      <div>
+      <div className="input-wrapper">
         <label htmlFor="address1">Address</label>
         <input type="text" name="address1" id="address1" />
       </div>
 
-      <div>
+      <div className="input-wrapper">
         <label htmlFor="address2">Address</label>
         <input type="text" name="address2" id="address2" />
       </div>
 
-      <div>
+      <div className="input-wrapper">
         <label htmlFor="city">City</label>
         <input type="text" name="city" id="city" />
       </div>
 
-      <div>
+      <div className="input-wrapper">
         <label htmlFor="province">Province</label>
         <input type="text" name="province" id="province" />
       </div>
 
-      <div>
+      <div className="input-wrapper">
         <label htmlFor="country">Country</label>
         <input type="text" name="province" id="country" />
       </div>
 
-      <div>
+      <div className="input-wrapper">
         <label htmlFor="postal">Postal Code</label>
         <input type="text" name="postal" id="postal" />
       </div>
 
-      <div>
+      <div className="input-wrapper">
         {/*
           TODO: Add JS functionality for pre-checked
           React requires event handlers for this state change
         */}
         <input type="checkbox" id="consent" defaultChecked />
-        <label htmlFor="consent">Consent to bureau check</label>
+        <label className="consent-label" htmlFor="consent">Consent to bureau check</label>
       </div>
       <div>
         <input type="submit" value="Let's Talk" />
